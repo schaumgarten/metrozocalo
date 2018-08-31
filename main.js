4ebc8e028665d382a51ed2f3199ee4f98476b4c9
Schaum <schaumgarten@gmail.com>
2018-08-30 20:25:22 -0500

    objetos convertidos en clases y funcion generadora de objetos al azar

--- main.js
+++ main.js
@@ -5,10 +5,14 @@ var xPositionNext=0;
 var yPositionNext=0;
 var travelLog = [];
 var numberOfSlots = 9;
-var verticalTrack = {
-    id : 1,
-    position: [2,0], 
-    task: function(previousX, previousY){
+var piecesArray = [];
+
+class VerticalTrack {
+    constructor(){
+        this.id = 1;
+        this.position = []
+    }
+   task(previousX, previousY) {
         xPositionCurrent = this.position[0];
         yPositionCurrent = this. position[1];
         if (previousY < this.position[1]){
@@ -20,10 +24,12 @@ var verticalTrack = {
         }
     }
 }
-var horizontalTrack = {
-    id : 2,
-    position: [0,0], 
-    task: function(previousX, previousY){
+class HorizontalTrack {
+    constructor() {
+        this.id = 2;
+        this.position = []
+    }
+    task(previousX, previousY) {
         xPositionCurrent = this.position[0];
         yPositionCurrent = this. position[1];
         if (previousX < this.position[0]){
@@ -36,10 +42,13 @@ var horizontalTrack = {
     }
 }
 
-var crossingTrack = {
-    id : 3,
-    position: [1,1],
-    task: function(previousX,previousY){
+class CrossingTrack {
+    constructor(){
+        this.id = 3;
+        this.position = []
+    }
+    
+    task(previousX,previousY) {
         xPositionCurrent = this.position[0];
         yPositionCurrent = this. position[1];
         if (previousX < this.position[0]){
@@ -58,10 +67,12 @@ var crossingTrack = {
     }
 }
 
-var upRightTrack = {
-    id : 4,
-    position: [0,2],
-    task : function(previousX,previousY){
+class UpRightTrack {
+    constructor(){
+        this.id = 4;
+        this.position = []
+    }
+    task(previousX,previousY) {
         xPositionCurrent = this.position[0];
         yPositionCurrent = this. position[1];
         if (previousX > this.position [0]){
@@ -74,10 +85,12 @@ var upRightTrack = {
     }
 } 
 
-var downRightTrack = {
-    id : 5,
-    position: [0,1],
-    task : function(previousX,previousY){
+class DownRightTrack {
+    constructor(){
+        this.id = 5;
+        this.position = []
+    }
+    task(previousX,previousY) {
         xPositionCurrent = this.position[0];
         yPositionCurrent = this. position[1];
         if (previousX > this.position [0]){
@@ -90,10 +103,12 @@ var downRightTrack = {
     }
 } 
 
-var downLeftTrack = {
-    id : 6,
-    position: [1,0],
-    task : function(previousX,previousY){
+class DownLeftTrack {
+    constructor(){
+        this.id = 6;
+        this.position = []
+    }
+    task(previousX,previousY){
         xPositionCurrent = this.position[0];
         yPositionCurrent = this. position[1];
         if (previousX < this.position [0]){
@@ -106,10 +121,11 @@ var downLeftTrack = {
     }
 } 
 
-var upLeftTrack = {
-    id : 7,
-    position: [1,2],
-    task : function(previousX,previousY){
+class UpLeftTrack {constructor(){
+    this.id = 7;
+    this.position = []
+}
+    task(previousX,previousY) {
         xPositionCurrent = this.position[0];
         yPositionCurrent = this. position[1];
         if (previousX < this.position [0]){
@@ -122,23 +138,80 @@ var upLeftTrack = {
     }
 } 
 
+// let horizontal = new HorizontalTrack;
+// horizontal.position.push(0,0);
+// cuadricula[0].splice(0,1,horizontal);
+// let turnDown = new DownLeftTrack;
+// turnDown.position.push(1,0);
+// cuadricula[1].splice(0,1,turnDown);
+// let crossing = new CrossingTrack;
+// crossing.position.push(1,1);
+// cuadricula[1].splice(1,1,crossing);
+// let turnLeft = new UpLeftTrack;
+// turnLeft.position.push(1,2);
+// cuadricula[1].splice(2,1,turnLeft);
+// let turnUp = new UpRightTrack;
+// turnUp.position.push(0,2);
+// cuadricula[0].splice(2,1,turnUp);
+// let turnRight = new DownRightTrack;
+// turnRight.position.push(0,1);
+// cuadricula[0].splice(1,1,turnRight);
+// let vertical = new VerticalTrack;
+// vertical.position.push(2,0);
+// cuadricula[2].splice(0,1,vertical);
+// console.log(cuadricula);
 
-cuadricula = [[horizontalTrack,downRightTrack,upRightTrack],[downLeftTrack,crossingTrack,upLeftTrack],[verticalTrack,"",""]];
-function  tracePath(valorX,valorY){
-    var piece = cuadricula[valorX][valorY];
-    if(typeof piece == 'object'){
-        piece.task(xPositionCurrent,yPositionCurrent);
-    } else {
-        return false;
+
+// //cuadricula = [[horizontalTrack,downRightTrack,upRightTrack],[downLeftTrack,crossingTrack,upLeftTrack],["","",""]];
+// function  tracePath(valorX,valorY){
+//     var piece = cuadricula[valorX][valorY];
+//     if(typeof piece == 'object'){
+//         piece.task(xPositionCurrent,yPositionCurrent);
+//     } else {
+//         return false;
+//     }
+// }
+
+// for (var i = 0; i<numberOfSlots; i++){
+//     var fun = tracePath(xPositionNext,yPositionNext);
+//     if (fun == false){
+//         break;
+//     } else {
+//         travelLog.push(xPositionCurrent,yPositionCurrent);
+//     }
+// }
+// console.log(travelLog);
+
+function getRandomPiece(){
+    var randomNumber = Math.floor((Math.random()*6)+1);
+    console.log("holi")
+    switch(randomNumber){
+        case 1:
+            return new VerticalTrack;
+        case 2:
+            return new HorizontalTrack;
+        case 3:
+            return new CrossingTrack;
+        case 4:
+            return new UpRightTrack;
+        case 5:
+            return new DownRightTrack;
+        case 6:
+            return new DownLeftTrack;
+        case 7:
+            return new UpLeftTrack;
+
     }
 }
 
-for (var i = 0; i<numberOfSlots; i++){
-    var fun = tracePath(xPositionNext,yPositionNext);
-    if (fun == false){
-        break;
-    } else {
-        travelLog.push(xPositionCurrent,yPositionCurrent);
+function formArray(){
+    for (var i = 1; i <= 5; i++){
+        piecesArray.push(getRandomPiece())
     }
 }
-console.log(travelLog);
+
+formArray();
+console.log(piecesArray);
+
+// console.log(cuadricula);
+
